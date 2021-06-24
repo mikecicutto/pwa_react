515f6812eece1bd15f3e97b994fb636ce9dc0867
Michael Cicutto <Michael.cicutto@gmail.com>
2021-06-23 20:52:28 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
+import { fetchWeather } from './api/fetchWeather'
+import './App.css'
+
 const App = () => {
+    const [ query, setQuery ] = useState('');
+
+    const search = async (e) => {
+        
+    }
+
     return (
-        <h1>App</h1>
+        <div className="main-container">
+            <input 
+                type="text"
+                className="search"
+                placeholder="Search..."
+                value={}
+                onChange={(e) => setQuery(e.target.value)}
+            />
+        </div>
     );
 }
 
